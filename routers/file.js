59ba2b0d6b0bd43f1c0743c7d3a1054d813178c5
neoxr <admin@neoxr.my.id>
2025-01-09 23:48:16 +0000

    chore: improve pipe handling

--- routers/file.js
+++ routers/file.js
@@ -11,22 +11,37 @@ export const routes = {
       try {
          const { token } = req.params
          const decoded = Jwt.verify(token, process.env.JWT_SECRET)
-         const fileUrl = decoded.url
-         const stream = await got.stream(fileUrl)
+         const url = decoded?.url
+         const cookie = decoded?.cookie
+         if (cookie) {
+            var stream = await got.stream(url, {
+               headers: {
+                  cookie
+               }
+            })
+         } else {
+            var stream = await got.stream(url)
+         }
          stream.on('error', e => {
             res.status(500).send(e.message)
          })
          stream.on('response', (response) => {
-            res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-            res.setHeader('Pragma', 'no-cache')
-            res.setHeader('Expires', '0')
-            res.setHeader('Content-Type', response.headers['content-type'] || 'application/octet-stream')
-            res.setHeader('Content-Length', response.headers['content-length'] || 0)
-            const filename = 'neoxr-' + Func.makeId(6) + '.' + (extension(response.headers['content-type']) || 'bin')
-            res.setHeader('Content-Disposition', 'attachment; filename=' + filename)
+            if (response?.statusCode !== 200) {
+               res.status(response.statusCode).send('Failed to fetch file')
+               stream.destroy()
+               return
+            }
+            res.set('Content-Type', response.headers['content-type'] || 'application/octet-stream')
+            res.set('Content-Length', response.headers['content-length'] || 0)
+            const filename = Func.makeId(6) + '.' + (extension(response.headers['content-type']) || 'bin')
+            res.set('Content-Disposition', 'attachment; filename=' + filename)
+            res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+            res.set('Pragma', 'no-cache')
+            res.set('Expires', '0')
          })
          stream.pipe(res).on('error', e => {
             res.status(500).send(e.message)
+            res.destroy(e)
          })
       } catch (e) {
          res.status(500).send(e.message)
