2816966d82f0d3cf04fbb3f42eb7d4d7a41b1bcd
neoxr <admin@neoxr.my.id>
2025-01-09 10:05:55 +0000

    update

--- routers/file.js
+++ routers/file.js
@@ -1,8 +1,6 @@
 import { Function as Func } from '../lib/index.js'
 import { extension } from 'mime-types'
 import Jwt from 'jsonwebtoken'
-import fs from 'fs'
-import path from 'path'
 import { got } from 'got'
 
 export const routes = {
@@ -16,35 +14,23 @@ export const routes = {
          const fileUrl = decoded.url
          const stream = await got.stream(fileUrl)
          stream.on('error', e => {
-            sendErrorFile(res, e.message)
+            res.status(500).send(e.message)
          })
          stream.on("response", (response) => {
+            res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate")
+            res.setHeader("Pragma", "no-cache")
+            res.setHeader("Expires", "0")
             res.setHeader("Content-Type", response.headers["content-type"] || "application/octet-stream")
-            res.setHeader("Content-Length", response.headers["content-length"])
+            res.setHeader("Content-Length", response.headers["content-length"] || 0)
             const filename = 'neoxr-' + Func.makeId(6) + '.' + (extension(response.headers['content-type']) || 'bin')
             res.setHeader("Content-Disposition", "attachment; filename=" + filename)
          })
          stream.pipe(res).on('error', e => {
-            sendErrorFile(res, e.message)
+            res.status(500).send(e.message)
          })
       } catch (e) {
-         sendErrorFile(res, 'Token Expired!')
+         res.status(500).send(e.message)
       }
    },
    error: false
-}
-
-const sendErrorFile = (res, errorMessage) => {
-   const errorFilePath = path.join(process.cwd(), 'error.txt')
-
-   // Menulis error ke file .txt
-   fs.writeFileSync(errorFilePath, errorMessage)
-
-   // Mengirim file error ke klien
-   res.status(500).sendFile(errorFilePath, (err) => {
-      if (err) {
-         console.error("Error sending error file:", err.message)
-         res.status(500).send("Error sending error file")
-      }
-   })
 }
