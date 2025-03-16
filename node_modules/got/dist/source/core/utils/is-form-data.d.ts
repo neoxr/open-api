9d77c9ba9f4732a9b658b77d4c72545a382acb9c
Wildan Izzudin <neoxr@null.net>
2025-03-16 21:36:06 +0000

    remove node_modules

--- node_modules/got/dist/source/core/utils/is-form-data.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Readable } from 'node:stream';
-type FormData = {
-    getBoundary: () => string;
-    getLength: (callback: (error: Error | null, length: number) => void) => void;
-} & Readable;
-export default function isFormData(body: unknown): body is FormData;
-export {};
