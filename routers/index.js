dad8e002fd8c569b8b3cfdbabc5bae3c7aff731b
neoxr <admin@neoxr.my.id>
2025-03-17 05:10:41 +0000

    update

--- routers/index.js
+++ routers/index.js
@@ -3,11 +3,20 @@ export const routes = {
    path: '/',
    method: 'get',
    execution: async (req, res, next) => {
-      res.json({
-         creator: global.creator,
-         status: true,
-         msg: 'Hello World ^_^'
-      })
+      try {
+         const json = await (await fetch('http://ip-api.com/json')).json()
+         res.json({
+            creator: global.creator,
+            status: true,
+            data: json
+         })
+      } catch (e) {
+         res.json({
+            creator: global.creator,
+            status: false,
+            msg: e.message
+         })
+      }
    },
    error: false
 }
