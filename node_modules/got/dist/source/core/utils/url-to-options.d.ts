359ac1ddd45868346074c3d3d3b519bc5895d0a3
xnombeta <xnombeta@gmail.com>
2025-03-17 03:44:47 +0000

    update

---
+++ node_modules/got/dist/source/core/utils/url-to-options.d.ts
@@ -0,0 +1,14 @@
+import type { UrlWithStringQuery } from 'node:url';
+export type LegacyUrlOptions = {
+    protocol: string;
+    hostname: string;
+    host: string;
+    hash: string | null;
+    search: string | null;
+    pathname: string;
+    href: string;
+    path: string;
+    port?: number;
+    auth?: string;
+};
+export default function urlToOptions(url: URL | UrlWithStringQuery): LegacyUrlOptions;
