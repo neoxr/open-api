a66469c344fd77b62b8af18415adb4fd36766325
Wildan Izzudin <admin@neoxr.my.id>
2025-06-23 21:26:44 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',      // agar bisa diakses dari jaringan lokal
     port: 3000,           // port yang diinginkan
-    open: true,           // otomatis buka di browser
+    open: false,           // otomatis buka di browser
     https: false,         // true jika ingin jalankan dengan HTTPS
     watch: {
       usePolling: true
