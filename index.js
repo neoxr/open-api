adf0ee5a4d8e298fd7990ca72fe75795718170ce
neoxr <neoxr@null.net>
2025-10-25 06:27:23 +0000

    chore(release): bump @neoxr/webly from 0.0.1 to 0.0.2
    feat: integrate WebSocket for real-time data and add custom error page

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import Webly, { App, Loader } from '@neoxr/webly'
 import middleware from './lib/system/middleware.js'
-
+import os from 'node:os'
 await Loader.scraper('./lib/scraper')
 
 const app = new App({
    name: 'Open-API',
-   staticPath: './public',
+   staticPath: ['public'],
    routePath: './routers',
    middleware,
-   port: 3000
+   socket: true,
+   port: 3001,
+   error: (req, res) => {
+      res.status(404).sendFile('./public/404.html', { root: process.cwd() })
+   }
+})
+
+app?.socket?.on('connection', (socket) => {
+   console.log(`Client connected: ${socket.id}`);
+
+   const systemMonitorInterval = setInterval(() => {
+      const totalMemory = os.totalmem()
+      const freeMemory = os.freemem()
+      const usedMemory = totalMemory - freeMemory
+      const usagePercentage = (usedMemory / totalMemory) * 100
+
+      const formattedData = {
+         total: `${(totalMemory / 1024 / 1024).toFixed(2)} MB`,
+         free: `${(freeMemory / 1024 / 1024).toFixed(2)} MB`,
+         used: `${(usedMemory / 1024 / 1024).toFixed(2)} MB`,
+         usagePercentage: usagePercentage.toFixed(2)
+      }
+
+      socket.emit('system_update', formattedData)
+
+   }, 2000)
+
+   socket.on('disconnect', () => {
+      console.log(`Client disconnected: ${socket.id}`)
+      clearInterval(systemMonitorInterval)
+   })
 })
 
 app.start()
